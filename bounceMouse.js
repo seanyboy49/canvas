63d08e6b27f21211ad2c83032d64f86808c817ad
sean <seanboramlee@gmail.com>
2020-02-16 21:35:38 -0800

    add bounceMouse.js and add margin to canvasWidth and canvasHeight

---
+++ bounceMouse.js
@@ -0,0 +1,89 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+const { innerWidth, innerHeight } = window
+
+const canvasMargin = 20
+const canvasWidth = innerWidth - canvasMargin * 2
+const canvasHeight = innerHeight - 200 - canvasMargin * 2
+// Set canvas dimensions
+canvas.width = canvasWidth
+canvas.height = canvasHeight
+
+window.addEventListener("mousemove", () => {
+  console.log("mouse move")
+})
+
+class Circle {
+  constructor({ x, y, directionX, directionY, radius, color = "black" }) {
+    this.x = x
+    this.y = y
+    this.directionX = directionX
+    this.directionY = directionY
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+    // ctx.stroke()
+  }
+
+  update() {
+    this.draw()
+    // If the circle reaches the sides, change direction
+    if (this.x >= canvasWidth - this.radius || this.x <= 0 + this.radius) {
+      this.directionX = -this.directionX
+    }
+    // If the circles reaches the ceiling or floor, change direction
+    if (this.y >= canvasHeight - this.radius || this.y <= 0 + this.radius) {
+      this.directionY = -this.directionY
+    }
+
+    // Increment x and y by direction
+    this.x += this.directionX
+    this.y += this.directionY
+  }
+}
+
+function generateCircles(number) {
+  const circles = []
+  for (let i = 0; i < number; i++) {
+    // initialize radius, x, y and x and y velocities
+    const radius = 50
+    let x = Math.random() * (canvasWidth - radius * 2) + radius // radius * 2 = circumference
+    // Math.random() - 0.5 will give you a random negative or positive value
+    let directionX = (Math.random() - 0.5) * 10
+    let y = Math.random() * (canvasHeight - radius * 2) + radius
+    let directionY = (Math.random() - 0.5) * 10
+
+    const circle = new Circle({
+      x,
+      y,
+      directionX,
+      directionY,
+      radius
+    })
+
+    circles.push(circle)
+  }
+  return circles
+}
+
+const circles = generateCircles(50)
+
+function classCircleAnimate() {
+  // requestAnimationFrame creates a loop that calls animate
+  // Animation works by refreshing the page, and on each refresh,
+  // we move our item incrementally which creates the illusion
+  // of animation
+  requestAnimationFrame(classCircleAnimate)
+  // Clear the entire canvas so that new circles don't overlap with previous circles
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+  circles.forEach(circle => circle.update())
+}
+
+classCircleAnimate()
